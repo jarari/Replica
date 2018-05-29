36446bc86e13666c002a58c6061a162b9b3394d3
jarari <hazardhax11@gmail.com>
2018-05-28 20:28:04 -0700

    Forgot to add secbot2 classes

---
+++ Assets/Scripts/GameData/Weapons/WeaponSecbot2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/* 시큐리티 봇 2 무기 클래스 */
+public class WeaponSecbot2 : Weapon {
+    public override void OnAttack(string eventname) {
+        base.OnAttack(eventname);
+    }
+
+    public override void OnWeaponEvent(string eventname) {
+    }
+}
