01b5afda4c5fb0229d0746c7d23828a01874743a
BuildTools <unconfigured@null.spigotmc.org>
2018-07-13 03:28:04 +0900

    particle&sstats

--- Assets/Scripts/GameData/Characters/CharacterSecbot.cs
+++ Assets/Scripts/GameData/Characters/CharacterSecbot.cs
@@ -16,7 +16,9 @@ public class CharacterSecbot : Character {
         EffectManager.instance.CreateEffect("effect_character_secbot_death", transform.position, GetFacingDirection());
         EffectManager.instance.CreateEffect("effect_character_secbot_explosion", transform.position, 0);
         ParticleManager.instance.CreateParticle("particle_secbot_death", transform.position, 0);
-        ParticleManager.instance.CreateParticle("particle_secbot_death2", transform.position, 0);
+        ParticleManager.instance.CreateParticle("particle_secbot_shard", transform.position, 0);
+        ParticleManager.instance.CreateParticle("particle_secbot_shard1", transform.position, 0);
+        ParticleManager.instance.CreateParticle("particle_secbot_shard2", transform.position, 0);
         base.OnDeath();
     }
 
