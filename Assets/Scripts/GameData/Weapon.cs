876a860d796619477781e7dafc6acc9a1d6e9f5e
Aduare <32541857+Aduare@users.noreply.github.com>
2018-09-19 02:37:32 +0900

    newparticletest

--- Assets/Scripts/GameData/Weapon.cs
+++ Assets/Scripts/GameData/Weapon.cs
@@ -186,6 +186,7 @@ public class Weapon : ObjectBase {
         if(actualEnemiesHit.Count > 0) {
             if (GameDataManager.instance.GetData(className, "Sprites", "hit") != null)
                 EffectManager.instance.CreateEffect((string)GameDataManager.instance.GetData(className, "Sprites", "hit"), avgHitPos, owner.GetFacingDirection());
+				ParticleManager.instance.CreateParticle("Particle_enemyhit",  avgHitPos, owner.GetFacingDirection());
         }
     }
 
