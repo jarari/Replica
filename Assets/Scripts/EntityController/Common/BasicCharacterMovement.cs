3fb2649bb087dab2ff984a3a8283bf2403706800
Aduare <32541857+Aduare@users.noreply.github.com>
2018-10-01 22:30:25 +0900

    secdroideffect

--- Assets/Scripts/EntityController/Common/BasicCharacterMovement.cs
+++ Assets/Scripts/EntityController/Common/BasicCharacterMovement.cs
@@ -222,7 +222,8 @@ public class BasicCharacterMovement : MoveObject {
             EffectManager.instance.CreateEffect("effect_tumble_back2", transform.position, 0, null, !character.IsFacingRight());
             EffectManager.instance.CreateEffect("effect_tumble_backf", transform.position, 0, transform, !character.IsFacingRight());
             ParticleManager.instance.CreateParticle("particle_tumb", transform.position, (int)Mathf.Sign(transform.localScale.x), transform);
-            ParticleManager.instance.CreateParticle("particle_tumb1", transform.position, (int)Mathf.Sign(transform.localScale.x), transform);
+			ParticleManager.instance.CreateParticle("particle_tumbb", transform.position, (int)Mathf.Sign(transform.localScale.x), transform);
+			ParticleManager.instance.CreateParticle("particle_tumbb", transform.position, (int)Mathf.Sign(transform.localScale.x), !character.IsFacingRight());
         }
         else if(character.GetAnimator().GetCurrentAnimatorStateInfo(0).IsTag("tumble_front"))
         {
@@ -230,7 +231,8 @@ public class BasicCharacterMovement : MoveObject {
             EffectManager.instance.CreateEffect("effect_tumble_front2", transform.position, 0, null, !character.IsFacingRight());
             EffectManager.instance.CreateEffect("effect_tumble_frontf", transform.position, 0, transform, !character.IsFacingRight());
             ParticleManager.instance.CreateParticle("particle_tumb", transform.position, (int)Mathf.Sign(transform.localScale.x), transform);
-            ParticleManager.instance.CreateParticle("particle_tumb2", transform.position, (int)Mathf.Sign(transform.localScale.x), transform);
+			ParticleManager.instance.CreateParticle("particle_tumbf", transform.position, (int)Mathf.Sign(transform.localScale.x), transform);
+			ParticleManager.instance.CreateParticle("particle_tumbf", transform.position, (int)Mathf.Sign(transform.localScale.x), !character.IsFacingRight());
         }
 
         lastDash = Time.time;
